a144ea04aabbe38268f2e3fd0fb414dcb7c59181
Mark Howe <mvahowe@gmail.com>
2025-05-06 13:53:28 +0200

    Fix off by ones and type issues

--- scripts/mergeJuxtas.js
+++ scripts/mergeJuxtas.js
@@ -9,9 +9,8 @@ const fromJuxta = fse.readJsonSync(process.argv[2]);
 const fromJuxtaJson = fromJuxta.bookCode ? fromJuxta.sentences : fromJuxta;
 const toJuxta = fse.readJsonSync(process.argv[3]);
 let toJuxtaJson = toJuxta.bookCode ? toJuxta.sentences : toJuxta;
-const fromStart = process.argv[4] - 1;
-const fromEnd = process.argv[5] - 1;
-
+const fromStart = parseInt(process.argv[4]) - 1;
+const fromEnd = parseInt(process.argv[5]) - 1;
 
 for (const [fromSentenceN, sentence] of [...fromJuxtaJson.entries()].slice(fromStart, fromEnd)) {
     toJuxtaJson[fromSentenceN + fromStart] = sentence;
